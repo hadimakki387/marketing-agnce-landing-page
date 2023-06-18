dadb4d33a8b373264eb928fb8d1b19641569d910
hadimakki387 <hmakki387@gmail.com>
2023-06-18 17:54:15 +0300

    changed svgs to pngs

--- src/components/Home/FirstDiv/FirstDiv.tsx
+++ src/components/Home/FirstDiv/FirstDiv.tsx
@@ -98,28 +98,28 @@ function FirstDiv() {
               >
                 <Image
                   className="img-fluid d-block animation-one snipcss0-5-21-22"
-                  src="/hero-animation-04.svg"
+                  src="/hero-animation-04.png"
                   alt="animation image"
                   width={500}
                   height={500}
                 />
                 <Image
                   className="img-fluid d-none d-lg-block animation-two snipcss0-5-21-23"
-                  src="/hero-animation-01.svg"
+                  src="/hero-animation-01.png"
                   alt="animation image"
                   width={120}
                   height={120}
                 />
                 <Image
                   className="img-fluid d-none d-lg-block animation-three snipcss0-5-21-24"
-                  src="/hero-animation-02.svg"
+                  src="/hero-animation-02.png"
                   alt="animation image"
                   width={120}
                   height={120}
                 />
                 <Image
                   className="img-fluid d-none d-lg-block animation-four snipcss0-5-21-25"
-                  src="/hero-animation-03.svg"
+                  src="/hero-animation-03.png"
                   alt="animation image"
                   width={230}
                   height={230}

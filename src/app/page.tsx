8d4baf45db95f4842c07578ea0ba8c5571f97de8
hadimakki387 <hmakki387@gmail.com>
2023-06-21 17:07:42 +0300

    fixed some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,60 @@
-import Index from "./Index";
+"use client";
+import "./css/SideBar.css";
+import "./css/New.css";
+import "./css/shortcuts.css";
+import FirstDiv from "@/components/Home/FirstDiv/FirstDiv";
+import SecondDiv from "@/components/Home/SecondDiv";
+import ThirdDiv from "@/components/Home/ThirdDiv";
+import FourthDiv from "@/components/Home/FourthDiv";
+import FifthDiv from "@/components/Home/FifthDiv";
+import SixthDiv from "@/components/Home/SixthDiv";
+import EightThDiv from "@/components/Home/EightThDiv";
+import Footer from "@/components/Home/Footer";
+import LazyLoad from "react-lazy-load";
+import { useEffect, useState } from "react";
+import Testemonials from "@/components/Home/Testemonials";
 
-export default function Home() {
+export default function Index() {
+  const [theme,setTheme]=useState(false)
+  function themeF(){
+    setTheme(!theme)
+  }
+
+  const [color,setColor] = useState("color-dark")
+  const [bg,setBg] = useState("bg-white")
+
+  useEffect(()=>{
+    setColor(theme===true?" color-white":" color-dark")
+    setBg(theme===true?" bg-dark": " bg-white")
+  },[theme])
+
   return (
-    <Index/>
+    <div className={"main " + bg}>
+      <LazyLoad >
+        <FirstDiv theme={color} themeF={themeF}/>
+      </LazyLoad>
+
+      <LazyLoad >
+        <SecondDiv theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <ThirdDiv theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <FourthDiv theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <FifthDiv theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <Testemonials theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <SixthDiv theme={color}/>
+      </LazyLoad>
+      <LazyLoad>
+        <Footer theme={color}/>
+      </LazyLoad>
+    </div>
   );
 }
